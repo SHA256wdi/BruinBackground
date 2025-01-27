6373e4c0ec488dc5450fb4c109546a0e2eb3c38e
SHA256wdi <shawgaze@gmail.com>
2025-01-26 21:16:32 -0800

    Initial Commit

---
+++ iOS101-Prework/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  iOS101-Prework
+//
+//  Created by Shaw on 10/24/24.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+        
+        func changeColor() -> UIColor{
+            
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+            
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+    }
+    
+}
+
